49ae097792bcfbad9878387d4e2537931a443d30
seiferli <469997798@qq.com>
2017-07-29 23:26:21 +0800

    update config

--- mgo/config.go
+++ mgo/config.go
@@ -1,6 +1,9 @@
 package mgo
 
-import "os"
+import (
+	"os"
+	"strconv"
+)
 
 type DbConfig struct {
 	host     string
@@ -11,6 +14,7 @@ type DbConfig struct {
 }
 
 func (conf *DbConfig) initConfig(id int) {
+	conf.poolsize = 50
 	if os.Getenv("ENV") == "pro" || os.Getenv("ENV") == "test" {
 		conf.host = os.Getenv("MONGO_HOST")
 		conf.port = os.Getenv("MONGO_PORT")
@@ -23,8 +27,11 @@ func (conf *DbConfig) initConfig(id int) {
 		conf.port = ini.GetValue("MONGODB", "MONGO_PORT")
 		conf.user = ini.GetValue("MONGODB", "MONGO_USER")
 		conf.passwd = ini.GetValue("MONGODB", "MONGO_PASSWD")
+		pool, err:= strconv.Atoi(ini.GetValue("MONGODB", "MONGO_POOLSIZE"))
+		if err!= nil && pool>1 {
+			conf.poolsize = pool
+		}
 	}
-	conf.poolsize = 15
 	//switch (id) {} //do some configuration
 }
 
